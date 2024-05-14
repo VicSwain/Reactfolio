92846955c5368eaea68cb241aea742d391a92349
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-05-14 11:28:36 -0500

    updated onBlur to apply dynamic text to prompt user to enter correct contact info requirement

--- Reactfolio/src/components/Project.jsx
+++ Reactfolio/src/components/Project.jsx
@@ -5,7 +5,7 @@ function Project({ title, description, imageURL }) {
           <div className="col-md-6">
             <h2>{title}</h2>
             <p>{description}</p>
-            <p><a className="btn btn-default" href="https://github.com/VicSwain/Hunter-s-Path" role="button">View details &raquo;</a></p>
+            <p><a className="btn btn-default" href="https://github.com/VicSwain/Hunter-s-Path" role="button" >View details &raquo;</a></p>
           </div>
           <div className="col-md-6">
           <h2>{title}</h2>
--- Reactfolio/src/pages/Resume.jsx
+++ Reactfolio/src/pages/Resume.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
   return (
   <div className="container d-flex justify-content-center align-items-center text-center">
-    <h1>Resume</h1>
+    <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#4CDA29', color: 'black' }}>Click here to download my resume</button>
     </div>
   )
 }
